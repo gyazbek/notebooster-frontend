b668988e7ea4123b3d932c88579b3ec958ab4cdf
mholmesjr <mholmesjr@users.noreply.github.com>
2015-05-25 19:42:57 -0400

    Notes for Sale page dummy notes being fetched and filtering ability configure

--- scripts/controllers/notesforsale.js
+++ scripts/controllers/notesforsale.js
@@ -1,31 +1,31 @@
 'use strict';
 
 angular.module('angularNoteboosterApp')
-  .controller('NotesForSaleCtrl', function ($scope,$stateParams, nbApiService, Validate) {
-    $scope.model = {'email':'', 'message':'', 'name':'', 'subject':''};
-  	$scope.complete = false;
+.controller('NotesForSaleCtrl', function ($scope,$http) {
+  init();
 
+  function init(){
+    $http.get('app/notes/notesPurchased.json').success(function(data) {
+      $scope.notes = data;
+      $scope.itemCount = data.length;
+    });
+  };
 
+// .controller('NotesForSaleCtrl', function ($scope,$stateParams, nbApiService, Validate) {
+  // $scope.model = {'email':'', 'message':'', 'name':'', 'subject':''};
+  // $scope.complete = false;
 
+  // if(typeof $stateParams.noteId !== 'undefined') {
+  //   alert('note id is here');
+  // }
 
-     if(typeof $stateParams.noteId !== 'undefined') {
-        alert('note id is here');
-    }
-
- 
-
-
-        
-        nbApiService.browseNotes( '','')
-        .then(function(data){
-          // success case
-          $scope.complete = true;
-          $scope.results = data.results;
-        },function(data){
-          // error case
-          $scope.errors = data;
-        });
-      
-      
-
-  });
+  // nbApiService.browseNotes( '','')
+  // .then(function(data){
+  //         // success case
+  //         $scope.complete = true;
+  //         $scope.results = data.results;
+  //       },function(data){
+  //         // error case
+  //         $scope.errors = data;
+  //       });
+});
