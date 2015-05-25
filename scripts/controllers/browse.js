89902bb56be98a2776a5d9cf84af5de83bb6237f
mholmesjr <mholmesjr@users.noreply.github.com>
2015-05-24 20:47:43 -0400

    updated some of the browse

--- scripts/controllers/browse.js
+++ scripts/controllers/browse.js
@@ -2,43 +2,20 @@
 
 angular.module('angularNoteboosterApp')
   .controller('BrowseCtrl', function ($scope,$stateParams, nbApiService, Validate) {
-    $scope.model = {'email':'', 'message':'', 'name':'', 'subject':''};
-  	$scope.complete = false;
+    init();
 
-
-
-        if(typeof $stateParams.schoolId !== 'undefined') {
+    if(typeof $stateParams.schoolId !== 'undefined') {
         alert('school id is here');
     }
-
-
-     if(typeof $stateParams.courseId !== 'undefined') {
+    if(typeof $stateParams.courseId !== 'undefined') {
         alert('course id is here');
     }
-
- 
-
-
-        
-        nbApiService.browseNotes( '','')
-        .then(function(data){
-          // success case
-          $scope.complete = true;
-          $scope.results = data.results;
-        },function(data){
-          // error case
-          $scope.errors = data;
-        });
-      
-      
-
 
     $scope.search = function(formData){
       $scope.errors = [];
       Validate.form_validation(formData,$scope.errors);
       if(!formData.$invalid){
-        
-        nbApiService.browseNotes( '','')
+        nbApiService.browseNotes('','')
         .then(function(data){
         	// success case
         	$scope.complete = true;
@@ -49,6 +26,20 @@ angular.module('angularNoteboosterApp')
         });
       
       }
-
+    }
+    
+    function init(){
+      $scope.model = {'email':'', 'message':'', 'name':'', 'subject':''};
+  	  $scope.complete = false;
+      
+      nbApiService.browseNotes( '','')
+      .then(function(data){
+        // success case
+        $scope.complete = true;
+        $scope.results = data.results;
+      },function(data){
+        // error case
+        $scope.errors = data;
+      });
     }
   });
