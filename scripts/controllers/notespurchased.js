6fa1f950b1769fee71f8a7fc2c8647592749ab82
mholmesjr <mholmesjr@users.noreply.github.com>
2015-05-25 20:13:37 -0400

    watchlist page having dummy notes being fetched and filtering ability configured

--- scripts/controllers/notespurchased.js
+++ scripts/controllers/notespurchased.js
@@ -1,13 +1,13 @@
 'use strict';
 
 angular.module('angularNoteboosterApp')
-  .controller('NotesPurchasedCtrl', function ($scope,$http) {
-    init();
+.controller('NotesPurchasedCtrl', function ($scope,$http) {
+  init();
   
-    function init(){
-      $http.get('app/notes/notesPurchased.json').success(function(data) {
-        $scope.notesPurchased = data;
-        $scope.itemCount = data.length;
-      });
-    }
-  });
+  function init(){
+    $http.get('app/notes/notesPurchased.json').success(function(data) {
+      $scope.notesPurchased = data;
+      $scope.itemCount = data.length;
+    });
+  }
+});
--- scripts/controllers/watchlist.js
+++ scripts/controllers/watchlist.js
@@ -1,31 +1,30 @@
 'use strict';
 
 angular.module('angularNoteboosterApp')
-  .controller('WatchlistCtrl', function ($scope,$stateParams, nbApiService, Validate) {
-    $scope.model = {'email':'', 'message':'', 'name':'', 'subject':''};
-  	$scope.complete = false;
+.controller('WatchlistCtrl', function ($scope,$http) {
+  init();
 
+  function init(){
+    $http.get('app/watchlist/watchlist.json').success(function(data) {
+      $scope.results = data;
+      $scope.itemCount = data.length;
+    }); 
+  }
+});
+// .controller('WatchlistCtrl', function ($scope,$stateParams, nbApiService, Validate) {
+//   $scope.model = {'email':'', 'message':'', 'name':'', 'subject':''};
+//   $scope.complete = false;
 
+//   if(typeof $stateParams.noteId !== 'undefined') {
+//     alert('note id is here');
+//   }
 
-
-     if(typeof $stateParams.noteId !== 'undefined') {
-        alert('note id is here');
-    }
-
- 
-
-
-        
-        nbApiService.browseNotes( '','')
-        .then(function(data){
-          // success case
-          $scope.complete = true;
-          $scope.results = data.results;
-        },function(data){
-          // error case
-          $scope.errors = data;
-        });
-      
-      
-
-  });
+//   nbApiService.browseNotes( '','')
+//   .then(function(data){
+//     // success case
+//     $scope.complete = true;
+//     $scope.results = data.results;
+//   }, function(data){
+//     // error case
+//     $scope.errors = data;
+//   });
