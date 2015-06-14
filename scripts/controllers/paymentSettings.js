734fca2a44a370879109135ccd60bc5bc8ea242e
mholmesjr <mholmesjr@users.noreply.github.com>
2015-06-14 03:29:45 -0400

    payment setting page finished

--- scripts/controllers/paymentSettings.js
+++ scripts/controllers/paymentSettings.js
@@ -12,4 +12,18 @@ angular.module('angularNoteboosterApp')
       	console.log('Failed');
       });
     };
+
+    $scope.getPaypalEmail = function(){
+      nbApiService.profile()
+      .then(function(data){
+        $scope.paypal_email = data.profile.paypal_email;
+      },function(data){
+
+        });
+    };
+
+    init();
+    function init(){
+      $scope.getPaypalEmail();
+    };
   });
