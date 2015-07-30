6a349bd84483a5580ffd354d57b5447be588c7e1
Gui Yazbek <gui@guiyazbek.com>
2015-07-30 04:14:11 -0400

    Redid the password reset process to show a more meaningful error message aswell as added the old password field which wasn't getting sent in the request.

--- scripts/controllers/passwordchange.js
+++ scripts/controllers/passwordchange.js
@@ -4,18 +4,31 @@ angular.module('angularNoteboosterApp')
   .controller('PasswordSettingsCtrl', function ($scope, nbApiService, Validate) {
     $scope.model = {'new_password1':'','new_password2':''};
   	$scope.complete = false;
+    $scope.failed = false;
     $scope.changePassword = function(formData){
-      $scope.errors = [];
+      $scope.errors = null;
+      $scope.complete = false;
+      $scope.failed = false;
+
+      if($scope.model.new_password1 != $scope.model.new_password2){
+        $scope.errors = {'new_password2':['Password does not match']};
+        return;
+      }
+    
       Validate.form_validation(formData,$scope.errors);
       if(!formData.$invalid){
-        nbApiService.changePassword($scope.model.new_password1, $scope.model.new_password2)
+        $scope.passwordChangePromise = nbApiService.changePassword($scope.model.new_password1, $scope.model.new_password2, $scope.model.old_password)
         .then(function(data){
         	// success case
           console.log (data.success);
         	$scope.complete = true;
         },function(data){
         	// error case
-        	$scope.errors = data;
+  	         if(angular.isDefined(data.non_field_errors)){
+              data = data.non_field_errors;
+            }
+            $scope.failed = true;
+            $scope.errors = data;
         });
       }
     }
