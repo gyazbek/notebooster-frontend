14f0aa3946e0573ee121a59499d86d5043cc2141
Gregor Richardson <gregor.com@mac.com>
2015-11-21 10:08:42 -0500

    Lots of HTML tweaks & changes to organization goal calculator

--- src/scripts/controllers/goalCalculator.js
+++ src/scripts/controllers/goalCalculator.js
@@ -1,9 +1,13 @@
 'use strict';
 
 angular.module('angularNoteboosterApp').controller('GoalCalculatorCtrl', function($scope) {
-	$scope.calc_donation_total = 300;
-	$scope.calc_note_price = 5;
-	$scope.calc_donation_split = 24;
+	$scope.calc_donation_total = 2000;
+	$scope.calc_note_price = 10;
+	$scope.calc_donation_split = 50;
 
 	$scope.salesRequired = function() { return $scope.calc_donation_total / ($scope.calc_note_price * ($scope.calc_donation_split/100)) };
+
+	$scope.calc_avg_note_sales = 12;
+	$scope.uploadsRequired = function () { return $scope.salesRequired() / $scope.calc_avg_note_sales };
+
 });
